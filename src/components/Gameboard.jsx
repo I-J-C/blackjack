38c87792ca11f6969c07964a84e3d45ffc55db51
Ilka Collado <student07@Ilka-Collado.home>
2022-04-25 18:13:15 -0400

    more A handling, and detects player blackJack and ends the hand

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -8,7 +8,7 @@ const shuffle = '/shuffle/';
 const Gameboard = (props) => {
     const axios = require('axios');
     const [count, setCount] = useState(0);
-    
+
     const [player, setPlayer] = useState([]);
     const [playerAceCount, setPlayerAceCount] = useState(0);
     const [playerValue, setPlayerValue] = useState(0);
@@ -17,7 +17,7 @@ const Gameboard = (props) => {
     const [dealerValue, setDealerValue] = useState(0);
     const [dealerAceCount, setDealerAceCount] = useState(0);
     const [dealerAceAdded, setDealerAceAdded] = useState(0);
-    const [handOver, setHandOver] = useState();
+    const [handOver, setHandOver] = useState(false);
     const [message, setMessage] = useState('');
     const [handActive, setHandActive] = useState(false);
     let nextCard = count;
@@ -30,26 +30,26 @@ const Gameboard = (props) => {
     //literally gives permission to copy-paste in document for use.
     function useInterval(callback, delay) {
         const savedCallback = useRef();
-      
+
         // Remember the latest callback.
         useEffect(() => {
-          savedCallback.current = callback;
+            savedCallback.current = callback;
         }, [callback]);
-      
+
         // Set up the interval.
         useEffect(() => {
-          function tick() {
-            savedCallback.current();
-          }
-          if (delay !== null) {
-            let id = setInterval(tick, delay);
-            return () => clearInterval(id);
-          }
+            function tick() {
+                savedCallback.current();
+            }
+            if (delay !== null) {
+                let id = setInterval(tick, delay);
+                return () => clearInterval(id);
+            }
         }, [delay]);
-      }
+    }
 
     const startHand = () => {
-        //start the game with 1 card for player then 1 for dealer then 1 for player
+        // start the game with 1 card for player then 1 for dealer then 1 for player
         hit(player);
         setTimeout(() => {
             hit(dealer);
@@ -57,6 +57,15 @@ const Gameboard = (props) => {
         setTimeout(() => {
             hit(player);
         }, 2000);
+    }
+
+    const checkBlackJack = () => {
+            if (player.length <= 2 &&  playerAceCount !== 0 && playerValue >= 11) {
+                setPlayerValue(value => 21);
+                setHandOver(value => true);
+                setPlayerStand(value => true);
+                setMessage(value => "BlackJack!");
+            }
     }
 
     const shuffleDeck = () => {
@@ -78,12 +87,12 @@ const Gameboard = (props) => {
                 // always executed
             });
     }
-    
+
     const drawCards = (amount) => {
         let cards = [];
-        for(let i=0; i<amount; i++){
-        cards.push(props.deck[nextCard+i]);
-        nextCard +=1;
+        for (let i = 0; i < amount; i++) {
+            cards.push(props.deck[nextCard + i]);
+            nextCard += 1;
         }
         setCount(count => count + 1);
         return cards;
@@ -92,10 +101,10 @@ const Gameboard = (props) => {
     const hit = (playerDealer) => {
         drawnCards = drawCards(1);
         drawnCards.forEach(element => {
-            if(playerDealer === player) {
+            if (playerDealer === player) {
                 setPlayer(player => [...player, element]);
                 setPlayerValue(playerValue => playerValue + cardValue(element.code, player));
-            }else if (playerDealer === dealer) {
+            } else if (playerDealer === dealer) {
                 dealerLength += 1;
                 dealerTotal += cardValue(element.code);
                 setDealer(dealer => [...dealer, element]);
@@ -114,19 +123,19 @@ const Gameboard = (props) => {
                 setPlayerAceCount(count => count + 1);
             } else {
                 setDealerAceCount(count => count + 1);
-                if (dealerTotal + 10 <= 21 || dealerLength === 0){
+                if (dealerTotal + 10 <= 21 || dealerLength === 0) {
                     dealerAce = true;
                     setDealerAceAdded(value => true);
                     value += 10;
                 }
             }
         } else {
-          value = parseInt(cardCode);
+            value = parseInt(cardCode);
         }
         return value;
-      }
+    }
 
-      const aceIncrease = () => {
+    const aceIncrease = () => {
         if (dealerAceCount !== 0) {
             let oldValue = dealerTotal;
             if (oldValue + 10 >= 17 && oldValue + 10 <= 21 && oldValue === dealerValue) {
@@ -155,11 +164,12 @@ const Gameboard = (props) => {
     }
 
     useInterval(() => {
-        if(playerStand && !handOver) {
+        if (playerStand && !handOver) {
             hit(dealer);
             dealerAce = aceIncrease();
             if (dealerTotal > 21 && dealerAce && dealerTotal - 10 < 21) {
                 setDealerValue(value => value - 10);
+                dealerAce = false;
                 setDealerAceAdded(value => false);
                 setHandOver(value => false);
             }
@@ -167,71 +177,79 @@ const Gameboard = (props) => {
     }, (dealerValue >= 17 || dealerAce === true) ? null : 1000);
 
 
-    
 
-      useEffect(
+
+    useEffect(
         () => {
-              nextCard = count;
-              if (dealerValue >= 17 || (dealerAceAdded === true && dealerLength >= 2)) {
-                  setMessage(message => "Dealer turn over!")
-                  setHandOver(oldValue => true);
-                //   if (dealerAce)
-                //  console.log('Ace added: ' , dealerAceAdded);
-              }
-              if (playerValue > 21) {
-                  setHandOver(oldValue => true);
-                  setMessage(message => "Player Bust!");
-              } else if (dealerValue > 21) {
-                  if (dealerAceAdded && dealerValue - 10 < 21) {
-                      setDealerValue(value => value - 10);
-                  }else{
-                  setHandOver(oldValue => true);
-                  setMessage(message => "Dealer Bust!");
-                  }
-              }
-              if (handOver === true) {
-                  setHandActive(oldValue => false);
+            nextCard = count;
+            if(player.length === 2) {
+                checkBlackJack();
+            }
+            if ((dealerValue >= 17 && dealerValue <= 21) || (dealerAceAdded === true && dealerLength >= 2)) {
+                setMessage(message => "Dealer turn over!")
+                setHandOver(oldValue => true);
+            }
+            if (playerValue > 21) {
+                setHandOver(oldValue => true);
+                setMessage(message => "Player Bust!");
+            } else if (dealerValue > 21) {
+                if (dealerAceAdded && dealerTotal - 10 < 21) {
+                    setDealerValue(value => value - 10);
+                    setDealerAceAdded(value => false);
+                    dealerAce = false;
+                } else {
+                    setHandOver(oldValue => true);
+                    setMessage(message => "Dealer Bust!");
+                }
+            }
+            if (handOver === true) {
+                setHandActive(oldValue => false);
                 //settle bets
-              }
+            }
 
-          }, [playerValue, dealerValue, dealerAceAdded, handOver]);
+        }, [playerValue, dealerValue, dealerAceAdded, handOver, player]);
 
     return (
         <div>
-            <button disabled={handActive} onClick={()=>{
+            <button disabled={handActive} onClick={() => {
                 if (props.deck.length - count <= 15) {
                     shuffleDeck();
                 }
                 setHandActive(oldValue => true);
                 resetHand();
                 startHand();
-                }}>Start Hand</button> 
-                {/* <button onClick={shuffleDeck} >Shuffle Cards</button> */}
-            
+            }}>Start Hand</button>
+
             <div className="message">{message}</div>
             <div className="dealerBoard">
-                <div className="dealer-value">Dealer Value: {dealerValue}{(dealerAceCount !== 0 && dealerValue+10 <= 17)? `(${dealerValue+10})`:""}</div>
-                <DealerHand dealer={dealer}/>
+                <div className="dealer-value">Dealer Value: {dealerValue}{(dealerAceCount !== 0 && dealerValue + 10 <= 17) ? `(${dealerValue + 10})` : ""}</div>
+                <DealerHand dealer={dealer} />
             </div>
             <div className="playerBoard">
-            
-            <div>Player Value: {playerValue}{(playerAceCount !== 0 && playerValue+10 <= 21)? `(${playerValue+10})`:""}</div>
-            <div className="actions">
-                <button disabled={playerStand||handOver} className="hitButton" onClick={() => {
-                    hit(player);
-                }}>Hit</button>
-                <button disabled={playerStand||handOver} className="standButton" onClick={()=>{
-                    setPlayerStand(oldValue => true);
-                    if (playerAceCount !== 0) {
-                        if (playerValue + 10 <= 21) {
-                            setPlayerValue(value => value + 10);
+
+                <div>Player Value: {playerValue}{(playerAceCount !== 0 && playerValue + 10 <= 21) ? `(${playerValue + 10})` : ""}</div>
+                <div className="actions">
+                    <button disabled={playerStand || handOver} className="hitButton" onClick={() => {
+                        hit(player);
+                    }}>Hit</button>
+                    <button disabled={playerStand || handOver} className="standButton" onClick={() => {
+                        setPlayerStand(oldValue => true);
+                        if (playerAceCount !== 0) {
+                            if (playerValue + 10 <= 21) {
+                                setPlayerValue(value => value + 10);
+                            }
                         }
-                    }
-                }}>Stand</button>
+                    }}>Stand</button>
+                    {/* <button disabled={playerStand || handOver} className="blackJack-button" onClick={()=>{
+                            debugger;
+                            checkBlackJack();
+                            debugger;
+                        
+                    }}>BlackJack Checker</button> */}
+                </div>
+                <Player player={player} />
             </div>
-                <Player player={player}/>
-            </div>
-            
+
         </div>
     )
 }
